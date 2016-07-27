27a7c6805215947d3efb116392c2cd4eca96a7c8
Jeffrey Meyers <jeff@m4>
2016-07-26 18:54:17 -0700

    working?

--- Models/naivebayes.py
+++ Models/naivebayes.py
@@ -2,26 +2,26 @@ from sklearn.naive_bayes import MultinomialNB, GaussianNB
 from sklearn import preprocessing
 
 NB = MultinomialNB
-NB = GaussianNB
+#NB = GaussianNB
 
 datakey = {
-    0: {"Near":12, "None":200, "Urgent":129487},
-    1: {"Yes":7, "No":0},
-    2: {"Yes":0, "No":12094}
+    0: {"Near":0, "None":2, "Urgent":7},
+    1: {"Yes":8, "No":1},
+    2: {"Yes":8, "No":1}
 }
 
 labelkey = {
-    "Party": 200,
-    "Pub": 500,
-    "Study": 10,
-    "TV": 3
+    "Party": 3,
+    "Pub": 4,
+    "Study": 5,
+    "TV": 6
 }
 
 rlabelkey = {
-    200: "Party",
-    500: "Pub",
-    10: "Study",
-    3: "TV"
+    3: "Party",
+    4: "Pub",
+    5: "Study",
+    6: "TV"
 }
 
 def wrapper_for_nb_in_sklearn(data, current_state_to_predict):
@@ -42,23 +42,24 @@ def wrapper_for_nb_in_sklearn(data, current_state_to_predict):
         for j, value in enumerate(row):
             dataset[i].append(datakey[j][value])
 
-    print factors
-    print dataset
+    #print factors
+    #print dataset
 
     # convert labels into integers using labelkey
     labels = []
     for state in states:
         labels.append(labelkey[state])
 
-    print
-    print states
-    print labels
+    #print
+    #print states
+    #print labels
 
     # convert predictions to integers using datakey
     predicts = []
     for i, pred in enumerate(current_state_to_predict):
         predicts.append(datakey[i][pred])
 
+    print dataset
     # fit data and labels to model
     clf = NB()
     clf.fit(dataset, labels)
